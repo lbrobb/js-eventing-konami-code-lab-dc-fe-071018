95098cc353dbd1ada255f0902a301f569be09d05
lbrobb <lbrobb@gmail.com>
2018-08-23 23:58:14 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,13 +2,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   let index = 0;
-  document.body.addEventListener('keydown', function(e) {
-    // console.log("-- keydown --");
-    // let keyNumber = (e.detail || e.which);
-    //
-    // if (keyNumber === 38) {
-    //   console.log("-- up --");
-    }
+  document.body.addEventListener('keydown', onKeyDownHandler(e) {
+    console.log("-- keydown --");
+    let keyNumber = (e.detail || e.which);
   });
   function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
