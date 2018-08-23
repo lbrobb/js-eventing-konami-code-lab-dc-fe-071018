ba18725a2fbe9f1af6b0aa10d027fde6afd5df46
lbrobb <lbrobb@gmail.com>
2018-08-23 23:53:50 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,27 +1,27 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // let index = 0;
-  // document.body.addEventListener('keydown', onKeyDownHandler(e) {
-  //   console.log("-- keydown --");
-  //   let keyNumber = (e.detail || e.which);
-  //
-  //   if (keyNumber === 38) {
-  //     console.log("-- up --");
-  //   }
-  // });
-  // function onKeyDownHandler(e) {
-  //   const key = parseInt(e.detail || e.which);
-  //   if (key === code[index]) {
-  //     index++;
-  //     if (index === code.length) {
-  //       alert("Hurray!");
-  //       index = 0;
-  //     }
-  //   } else {
-  //     index = 0
-  //   }
-  // })
+  let index = 0;
+  document.body.addEventListener('keydown', onKeyDownHandler(e) {
+    // console.log("-- keydown --");
+    // let keyNumber = (e.detail || e.which);
+    // 
+    // if (keyNumber === 38) {
+    //   console.log("-- up --");
+    }
+  });
+  function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which);
+    if (key === code[index]) {
+      index++;
+      if (index === code.length) {
+        alert("Hurray!");
+        index = 0;
+      }
+    } else {
+      index = 0
+    }
+  })
 };
 
 init();
